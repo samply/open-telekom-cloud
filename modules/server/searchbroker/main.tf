a5291dd108a765e20a519934b0aecb8fc76d0049
Martin Breu <breu_m@ukw.de>
2020-01-14 12:41:10 +0100

    Feature/eu negotiator (#30)
    SL-UI=3.5.4 and SL-Backend=3.4.7

--- modules/server/searchbroker/main.tf
+++ modules/server/searchbroker/main.tf
@@ -18,7 +18,8 @@ data "template_file" "searchbroker_ui_service" {
   template = file("${path.module}/searchbroker-ui.service.tmpl")
   vars = {
     version         = var.searchbroker-ui_version
-    negotiator_url  = "https://negotiator.germanbiobanknode.de"
+    negotiator_url  = "https://negotiator.bbmri-eric.eu"
+    locator_url     = terraform.workspace == "default" ? "https://samplelocator.bbmri.de" : format("https://samplelocator.%s.bbmri.de", terraform.workspace)
     auth_host       = "https://login.bbmri-eric.eu"
     mdr_api_url     = terraform.workspace == "default" ? "https://mdr.germanbiobanknode.de/v3/api/mdr" : format("https://mdr.%s.bbmri.de/v3/api/mdr", terraform.workspace)
     mdr_namespace   = "mdr16"
