2d603af842d4dc80f71c73a61d907c4f25999175
BjoernKrollGba <48796144+BjoernKrollGba@users.noreply.github.com>
2020-04-14 11:03:33 +0200

    Update angular to 1.0.0

--- modules/server/main.tf
+++ modules/server/main.tf
@@ -22,8 +22,8 @@ module "certbot" {
 
 module "searchbroker" {
   source                  = "./searchbroker"
-  searchbroker_version    = "5.1.0"
-  searchbroker-ui_version = "0.9.1"
+  searchbroker_version    = "angular-ui-0.0.4"
+  searchbroker-ui_version = "1.0.1"
 }
 
 module "mdr" {
